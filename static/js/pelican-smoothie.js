df1b33f33b64d26c691b6eac885bca92e861efb2
Dheepak Krishnamurthy <kdheepak89@gmail.com>
2015-08-04 01:36:32 -0600

    Force scroll to top if href='#page-top'

--- static/js/pelican-smoothie.js
+++ static/js/pelican-smoothie.js
@@ -8,13 +8,26 @@
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
+        if(  $anchor.attr('href') == '#page-top' ) {
+        $('html, body').stop().animate({
+            scrollTop: 0
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+        }
+        else {
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
+        }
     });
 });
 
+
+$("a[href='#top']").click(function() {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+  return false;
+});
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
